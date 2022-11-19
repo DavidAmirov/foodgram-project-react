c4e15a5b2078e6789427f785b55d911bc2889c25
Давид Амиров <david.talish@yandex.ru>
2022-11-19 14:18:00 +0300

    Отправка на ревью 2.2

--- backend/api/management/commands/load_ingredients.py
+++ backend/api/management/commands/load_ingredients.py
@@ -21,16 +21,3 @@ class Command(BaseCommand):
                 except IntegrityError:
                     print(f'Ингредиент {name} {measurement_unit}'
                           f'уже есть в базе')
-        file_name = 'recipes/data/tags.csv'
-        with open(file_name, 'r', encoding='utf-8') as file:
-            file_reader = csv.reader(file)
-            for row in file_reader:
-                name, color, slug = row
-                try:
-                    Tag.objects.get_or_create(
-                        name=name,
-                        color=color,
-                        slug=slug
-                    )
-                except IntegrityError:
-                    print(f'Тег {name} уже есть в базе')
