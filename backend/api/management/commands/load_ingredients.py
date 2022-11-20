9113bb1f9ab18c35db40411e87f150d7485f0210
Давид Амиров <david.talish@yandex.ru>
2022-11-20 11:14:28 +0300

    отправка на ревью 2.23

--- backend/api/management/commands/load_ingredients.py
+++ backend/api/management/commands/load_ingredients.py
@@ -8,7 +8,7 @@ from api.models import Ingredient, Tag
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        file_name = 'api/data/ingredients.csv'
+        file_name = 'recipes/data/ingredients.csv'
         with open(file_name, 'r', encoding='utf-8') as file:
             file_reader = csv.reader(file)
             for row in file_reader:
@@ -21,3 +21,16 @@ class Command(BaseCommand):
                 except IntegrityError:
                     print(f'Ингредиент {name} {measurement_unit}'
                           f'уже есть в базе')
+        file_name = 'recipes/data/tags.csv'
+        with open(file_name, 'r', encoding='utf-8') as file:
+            file_reader = csv.reader(file)
+            for row in file_reader:
+                name, color, slug = row
+                try:
+                    Tag.objects.get_or_create(
+                        name=name,
+                        color=color,
+                        slug=slug
+                    )
+                except IntegrityError:
+                    print(f'Тег {name} уже есть в базе')
